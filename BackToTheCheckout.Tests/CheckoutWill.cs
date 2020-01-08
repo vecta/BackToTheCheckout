8762bfb0c9e6dc0e252d122282d725ff10d9b837
Richard Hopwood <richard.hopwood@codurance.com>
2020-01-08 22:08:47 +0000

    2nd Passing Test

--- BackToTheCheckout.Tests/CheckoutWill.cs
+++ BackToTheCheckout.Tests/CheckoutWill.cs
@@ -9,7 +9,14 @@ namespace BackToTheCheckout.Tests
         public void ReturnAZeroTotalWhenNothingIsScanned()
         {
             var checkout = new Checkout();
-            Assert.That(checkout.Total, Is.EqualTo(0));
+            Assert.That(checkout.Total, Is.EqualTo(0m));
+        }
+        [Test]
+        public void ReturnItemPriceWhenASingleItemIsScanned()
+        {
+            var checkout = new Checkout();
+            checkout.Scan("A99");
+            Assert.That(checkout.Total, Is.EqualTo(0.5m));
         }
     }
 }
--- BackToTheCheckout/Checkout.cs
+++ BackToTheCheckout/Checkout.cs
@@ -1,7 +1,15 @@
-﻿namespace BackToTheCheckout
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BackToTheCheckout
 {
     public class Checkout
     {
-        public int Total => 0;
+        private readonly List<string> _basket = new List<string>();
+        public decimal Total => SumBasket();
+
+        private decimal SumBasket() { return _basket.Sum(GetPrice); }
+        private decimal GetPrice(string sku) { return 0.5m; }
+        public void Scan(string sku) { _basket.Add(sku); }
     }
 }
